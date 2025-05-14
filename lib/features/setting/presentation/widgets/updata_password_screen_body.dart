a6ae518a579b7712ff1dd6d563289a001fd5fd1d
hayam-tarek <hayamtareq28@gmail.com>
2025-05-14 21:17:43 +0300

    added password set successfully dialog

--- lib/features/setting/presentation/widgets/updata_password_screen_body.dart
+++ lib/features/setting/presentation/widgets/updata_password_screen_body.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tride/core/helpers/extensions.dart';
 import 'package:tride/core/helpers/measures.dart';
 import 'package:tride/core/theming/app_colors.dart';
 import 'package:tride/core/theming/app_text_styles.dart';
@@ -9,6 +13,10 @@ import 'package:tride/generated/l10n.dart';
 
 class UpdatePasswordScreenBody extends StatelessWidget {
   const UpdatePasswordScreenBody({super.key});
+
+  _onPressSetPassword(BuildContext context) async {
+    await passwordChangedDialog(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,10 +53,45 @@ class UpdatePasswordScreenBody extends StatelessWidget {
             bgColor: AppColors.orange,
             textColor: AppColors.white,
             text: S.of(context).set_password,
-            onPressed: () {},
+            onPressed: () => _onPressSetPassword(context),
           ),
         ],
       ),
+    );
+  }
+
+  Future<dynamic> passwordChangedDialog(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        Timer(const Duration(seconds: 3), () {
+          context.pop();
+        });
+        return AlertDialog(
+          insetPadding: const EdgeInsets.symmetric(horizontal: 24),
+          contentPadding: const EdgeInsets.all(21),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          backgroundColor: Colors.white,
+          icon: SizedBox(
+            width: 80.w,
+            height: 80.h,
+            child: FittedBox(
+              child: CircleAvatar(
+                backgroundColor: AppColors.green,
+                child: Icon(Icons.check_rounded, color: AppColors.white),
+              ),
+            ),
+          ),
+          content: Text(
+            S.of(context).password_set_successfully,
+            style: AppTextStyles.roboto16Medium,
+            textAlign: TextAlign.center,
+          ),
+        );
+      },
     );
   }
 }
