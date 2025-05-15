d129eb919b9b08d3dd3ff53287df0187820b8bec
hayam-tarek <hayamtareq28@gmail.com>
2025-05-15 18:31:09 +0300

    finished notification card temp

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -1,47 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tride/core/theming/app_colors.dart';
-import 'package:tride/core/theming/app_paddings.dart';
 import 'package:tride/core/theming/app_strokes.dart';
 import 'package:tride/core/theming/app_text_styles.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
-  final double? width;
   final Color? bgColor;
+  final Color? borderColor;
+  final Color? textColor;
   final double? xPadding;
   final double? yPadding;
   final TextStyle? textStyle;
-  final bool isLoading;
   final Function()? onPressed;
+  final bool isLoading;
 
   const CustomButton({
     super.key,
-    this.width,
     this.bgColor,
+    this.textColor,
     this.xPadding,
     this.yPadding,
     this.textStyle,
-    this.isLoading = false,
     required this.text,
     required this.onPressed,
+    this.borderColor,
+    this.isLoading = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: width ?? double.infinity,
+      width: double.infinity,
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
+          elevation: 0,
           backgroundColor: bgColor ?? AppColors.orange,
-          padding: EdgeInsets.symmetric(
-            horizontal: xPadding != null ? xPadding!.w : AppPaddings.gapPadding8,
-            vertical: yPadding != null ? yPadding!.h : AppPaddings.gapPadding8,
-          ),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.r),
-            side: BorderSide(color: AppColors.transparent, width: 1.5.sp),
+            borderRadius: BorderRadius.circular(32.r),
+            side: BorderSide(
+              color: borderColor ?? AppColors.transparent,
+              width: 1.5.sp,
+            ),
           ),
         ),
         child:
@@ -50,10 +51,20 @@ class CustomButton extends StatelessWidget {
                   child: SizedBox(
                     width: 24,
                     height: 24,
-                    child: CircularProgressIndicator(color: AppColors.white, strokeWidth: AppStrokes.strokeVlg),
+                    child: CircularProgressIndicator(
+                      color: AppColors.white,
+                      strokeWidth: AppStrokes.strokeVlg,
+                    ),
                   ),
                 )
-                : Text(text, style: textStyle ?? AppTextStyles.roboto16SemiBold(context)),
+                : Text(
+                  text,
+                  style:
+                      textStyle ??
+                      AppTextStyles.roboto16SemiBold(
+                        context,
+                      ).copyWith(color: textColor ?? AppColors.white),
+                ),
       ),
     );
   }
